ab5d889dfb23972c2b320825bc687c8ea59814c2
Volkan Nazmi Metin <volkan.nazmi.metin@gmail.com>
2017-09-30 01:56:47 +0300

    Added removeBy
    Removes items from an array of objects which returns true from the predicate operator.

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ const getPropertyValue = (propertyName = '') => (obj = Object) => {
 	return obj.hasOwnProperty(propertyName) ? obj[propertyName] : undefined;
 };
 
+const predicateDefault = (val) => {
+	return val | true;
+};
+
+const removeBy = (predicate = predicateDefault) => (howMany = 1) => (arr = Array(0)) => {
+	let counter = 0;
+	let arrClone = [...arr];
+	for (let i = 0; i < arrClone.length; i++) {
+		if (predicate(arrClone[i]) && (!howMany || counter < howMany)) {
+			counter++;
+			arrClone.splice(i, 1);
+		}
+		if (howMany && counter == howMany) {
+			break;
+		}
+	}
+	return arrClone;
+};
+
 module.exports = {
 	compose : compose,
 	getPropVal : getPropertyValue,
@@ -22,7 +41,8 @@ module.exports = {
 	mixobj : mixobj,
 	mixfn : mixfn,
 	filter : filter,
-	map : map
+	map : map,
+	removeBy : removeBy
 };
 
 
